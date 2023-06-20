6114dc35a5b1cf862805bd06c28290639f55c2a8
Gornutz <90406700+Gornutz@users.noreply.github.com>
2023-06-20 15:13:24 -0400

    Update ascii / author

--- test/mock/MockERC20.sol
+++ test/mock/MockERC20.sol
@@ -11,7 +11,6 @@ pragma solidity 0.8.19;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
-/// @author Blueberry protocol
 contract MockERC20 is ERC20("Mock Token", "MCK", 18) {
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
