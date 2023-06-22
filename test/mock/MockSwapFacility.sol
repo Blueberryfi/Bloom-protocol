5bb55042f6a2af11816a528a485139634027ceaa
Philippe Dumonet <philippe@dumo.net>
2023-06-23 01:31:12 +0200

    Silence unused parameter warning

--- test/mock/MockSwapFacility.sol
+++ test/mock/MockSwapFacility.sol
@@ -52,7 +52,7 @@ contract MockSwapFacility is IMockSwapFacility {
         ISwapRecipient(pswap.to).completeSwap(address(pswap.token), pswap.amount);
     }
 
-    function swap(address inToken, address outToken, uint256 inAmount, bytes32[] calldata proof) external {
+    function swap(address inToken, address outToken, uint256 inAmount, bytes32[] calldata) external {
         if (
             !(
                 (inToken == address(token0) && outToken == address(token1))
