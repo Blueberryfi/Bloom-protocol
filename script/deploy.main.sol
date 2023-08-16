de662c7ce4efb797b5a4af5dadf4677f8311a516
Gornutz <90406700+Gornutz@users.noreply.github.com>
2023-08-16 09:07:38 -0500

    deploy scripts

--- script/deploy.main.sol
+++ script/deploy.main.sol
@@ -18,38 +18,33 @@ import {MerkleWhitelist} from "../src/MerkleWhitelist.sol";
 import {BPSFeed} from "../src/BPSFeed.sol";
 import {BloomPool} from "../src/BloomPool.sol";
 import {SwapFacility} from "../src/SwapFacility.sol";
-import {MockERC20} from "../test/mock/MockERC20.sol";
 
 import {ISwapFacility} from "../src/interfaces/ISwapFacility.sol";
 import {IWhitelist} from "../src/interfaces/IWhitelist.sol";
 
 contract Deploy is Test, Script {
-    address internal constant DEPLOYER = 0x3031303BB07C35d489cd4B7E6cCd6Fb16eA2b3a1;
-    address internal constant TREASURY = 0xE4D701c6E3bFbA3e50D1045A3cef4797b6165119;
-    address internal constant EMERGENCY_HANDLER = 0x989B1a8EefaC6bF66a159621D49FaF4A939b452D;
+    address internal constant DEPLOYER = 0x21c2bd51f230D69787DAf230672F70bAA1826F67;
+    address internal constant TREASURY = 0xFdC004B6B92b45B224d37dc45dBA5cA82c1e08f2;
+    address internal constant EMERGENCY_HANDLER = 0x91797a79fEA044D165B00D236488A0f2D22157BC;
 
-    address internal constant UNDERLYING_TOKEN = 0xa1c511b3C5Be3C94089203845D6247D1696D7Fb9; //
-    address internal constant BILL_TOKEN = 0x106c5522A76818cEdf06E885E8a8A63eb6Cf2a4b; //bIB01
-    // MockERC20 internal UNDERLYING_TOKEN;
-    // MockERC20 internal BILL_TOKEN;
+    address internal constant UNDERLYING_TOKEN = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
+    address internal constant BILL_TOKEN = 0xCA30c93B02514f86d5C86a6e375E3A330B435Fb5; //bIB01
 
-    // address internal constant WHITELIST = 0x5E1D6c195d71255c6c574e373379d9610CFF277d;
-    address internal constant BPSFEED = 0x4C46dfc9e4cCe661d6E24FE49C43adb004423541;
 
     // chainlink feeds
-    address internal constant USDCUSD = 0xA2F78ab2355fe2f984D808B5CeE7FD0A93D5270E;
-    address internal constant IB01USD = 0xB677bfBc9B09a3469695f40477d05bc9BcB15F50;
+    address internal constant USDCUSD = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
+    address internal constant IB01USD = 0x32d1463EB53b73C095625719Afa544D5426354cB;
     
     bytes32 internal constant INITIALROOTBORROW = 0xabc6a38afc2e6c26bad45002703dd3bae47e41d24d13e71f8e61687633acc2ea;
     bytes32 internal constant INITIALROOTSWAP = 0xabc6a38afc2e6c26bad45002703dd3bae47e41d24d13e71f8e61687633acc2ea;
-    address internal constant INITIALOWNER = 0x3031303BB07C35d489cd4B7E6cCd6Fb16eA2b3a1;
+    address internal constant INITIALOWNER = 0x21c2bd51f230D69787DAf230672F70bAA1826F67;
 
     uint256 internal constant SPREAD = 0.0125e4; // 0.125%
     uint256 internal constant MIN_STABLE_VALUE = 0.999999e8;
-    uint256 internal constant MAX_BILL_VALUE = 107.60e8;
+    uint256 internal constant MAX_BILL_VALUE = 109.60e8;
     uint256 internal constant BPS = 1e4;
-    uint256 internal constant commitPhaseDuration = 1 hours;
-    uint256 internal constant poolPhaseDuration = 2 days;
+    uint256 internal constant commitPhaseDuration = 3 days;
+    uint256 internal constant poolPhaseDuration = 180 days;
     uint256 internal constant preHoldSwapTimeout = 7 days;
     
     // Aux
@@ -66,10 +61,9 @@ contract Deploy is Test, Script {
         vm.startBroadcast();
 
         // Deploy aux items
-        //_deployBillyToken();
         _deployMerkleWhitelistBorrow();
         _deployMerkleWhitelistSwap();
-        //_deployBPSFeed();
+        _deployBPSFeed();
 
         // Deploy protocol
         _deploySwapFacility();
@@ -78,48 +72,40 @@ contract Deploy is Test, Script {
         vm.stopBroadcast();
     }
 
-   // function _deployBillyToken() internal {
-   //     UNDERLYING_TOKEN = new MockERC20(6);
-   //     vm.label(address(UNDERLYING_TOKEN), "UnderlyingToken");
-   //     BILL_TOKEN = new MockERC20(18);
-   //     vm.label(address(BILL_TOKEN), "BillyToken");
-   // }
-
     function _deployMerkleWhitelistBorrow() internal {
         whitelistBorrow = new MerkleWhitelist(
             INITIALROOTBORROW,
             INITIALOWNER
         );
-        vm.label(address(whitelistBorrow), "MerkleWhitelistBorrow");
+        vm.label(address(whitelistBorrow), "MerkleWhitelist");
         console2.log("MerkleWhitelist deployed at:", address(whitelistBorrow));
         }
-
-        function _deployMerkleWhitelistSwap() internal {
-        whitelistSwap = new MerkleWhitelist(
-            INITIALROOTSWAP,
-            INITIALOWNER
-        );
-        vm.label(address(whitelistSwap), "MerkleWhitelistSwap");
-        console2.log("MerkleWhitelist deployed at:", address(whitelistSwap));
-        }    
     
-   //     function _deployBPSFeed() internal {
-   //     lenderReturnBpsFeed = new BPSFeed();
-   //     vm.label(address(lenderReturnBpsFeed), "BPSFeed");
-   //     console2.log("BPSFeed deployed at:", address(lenderReturnBpsFeed));
-   // }
+    function _deployMerkleWhitelistSwap() internal {
+    whitelistSwap = new MerkleWhitelist(
+        INITIALROOTSWAP,
+        INITIALOWNER
+    );
+    vm.label(address(whitelistSwap), "MerkleWhitelist");
+    console2.log("MerkleWhitelist deployed at:", address(whitelistSwap));
+    }    
+    
+        function _deployBPSFeed() internal {
+        lenderReturnBpsFeed = new BPSFeed();
+        vm.label(address(lenderReturnBpsFeed), "BPSFeed");
+        console2.log("BPSFeed deployed at:", address(lenderReturnBpsFeed));
+    }
     
 
     function _deploySwapFacility() internal {
 
-        uint256 deployerNonce = vm.getNonce(address(DEPLOYER));
+        uint256 deployerNonce = vm.getNonce(address(this));
 
         swap = new SwapFacility(
-            UNDERLYING_TOKEN,        // address(UNDERLYING_TOKEN), 
-            BILL_TOKEN,        // address(BILL_TOKEN),
+            UNDERLYING_TOKEN, 
+            BILL_TOKEN,
             USDCUSD,
             IB01USD,
-            // IWhitelist(address(WHITELIST)),         
             IWhitelist(address(whitelistSwap)),
             SPREAD,
             LibRLP.computeAddress(address(DEPLOYER), deployerNonce +1),
@@ -133,23 +119,22 @@ contract Deploy is Test, Script {
     function _deployBloomPool() internal {
 
         pool = new BloomPool(
-            UNDERLYING_TOKEN,    // address(UNDERLYING_TOKEN),
-            BILL_TOKEN,          // address(BILL_TOKEN),
-            // IWhitelist(address(WHITELIST)),           // 
+            UNDERLYING_TOKEN,
+            BILL_TOKEN,
             IWhitelist(address(whitelistBorrow)),
             address(swap),
             TREASURY,
-            BPSFEED,             // address(lenderReturnBpsFeed),
+            address(lenderReturnBpsFeed),
             EMERGENCY_HANDLER,
             50e4,
-            1.0e6,
+            1,
             commitPhaseDuration,
             preHoldSwapTimeout,
             poolPhaseDuration,
-            300,
-            3000,
-            "Term Bound Yield 6 month 2023-2-4",
-            "TBY-Feb4"
+            300, // 3%
+            0, // 0%
+            "Term Bound Yield 6 month 2024-2-11",
+            "TBY-feb1124"
         );
         console2.log("BloomPool deployed at:", address(pool));
     }
--- script/deploy.main.test.sol
+++ script/deploy.main.test.sol
@@ -23,26 +23,27 @@ import {ISwapFacility} from "../src/interfaces/ISwapFacility.sol";
 import {IWhitelist} from "../src/interfaces/IWhitelist.sol";
 
 contract Deploy is Test, Script {
+    address internal constant DEPLOYER = 0x3031303BB07C35d489cd4B7E6cCd6Fb16eA2b3a1;
     address internal constant TREASURY = 0xE4D701c6E3bFbA3e50D1045A3cef4797b6165119;
     address internal constant EMERGENCY_HANDLER = 0x989B1a8EefaC6bF66a159621D49FaF4A939b452D;
 
     address internal constant UNDERLYING_TOKEN = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
     address internal constant BILL_TOKEN = 0xCA30c93B02514f86d5C86a6e375E3A330B435Fb5; //bIB01
-    
+
 
     // chainlink feeds
     address internal constant USDCUSD = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
     address internal constant IB01USD = 0x32d1463EB53b73C095625719Afa544D5426354cB;
     
-    bytes32 internal constant INITIALROOT = 0xb179801aa7ae4f1e71e664f691b85625d68c7f104c5c64b08010f15a93668c7f;
-    address internal constant INITIALOWNER = 0x72d19f7c71a2bD5E61871B1D3dF7a45ae5Ec9582;
+    bytes32 internal constant INITIALROOT = 0xabc6a38afc2e6c26bad45002703dd3bae47e41d24d13e71f8e61687633acc2ea;
+    address internal constant INITIALOWNER = 0x3031303BB07C35d489cd4B7E6cCd6Fb16eA2b3a1;
 
-    uint256 internal constant SPREAD = 0.004e4; // 0.04%
+    uint256 internal constant SPREAD = 0.0125e4; // 0.125%
     uint256 internal constant MIN_STABLE_VALUE = 0.999999e8;
     uint256 internal constant MAX_BILL_VALUE = 107.60e8;
     uint256 internal constant BPS = 1e4;
-    uint256 internal constant commitPhaseDuration = 3 days;
-    uint256 internal constant poolPhaseDuration = 180 days;
+    uint256 internal constant commitPhaseDuration = 3 hours;
+    uint256 internal constant poolPhaseDuration = 2 days;
     uint256 internal constant preHoldSwapTimeout = 7 days;
     
     // Aux
@@ -95,7 +96,7 @@ contract Deploy is Test, Script {
             IB01USD,
             IWhitelist(address(whitelist)),
             SPREAD,
-            LibRLP.computeAddress(address(this), deployerNonce +1),
+            LibRLP.computeAddress(address(DEPLOYER), deployerNonce +1),
             MIN_STABLE_VALUE,
             MAX_BILL_VALUE
         );
@@ -114,14 +115,14 @@ contract Deploy is Test, Script {
             address(lenderReturnBpsFeed),
             EMERGENCY_HANDLER,
             50e4,
-            100.0e18,
+            1.0e6,
             commitPhaseDuration,
             preHoldSwapTimeout,
             poolPhaseDuration,
-            300,
-            3000,
-            "Term Bound Token 6 month 2023-12-31",
-            "TBT-Dec31"
+            300, // 3%
+            0, // 30%
+            "Term Bound Yield Test",
+            "TBY-Test"
         );
         console2.log("BloomPool deployed at:", address(pool));
     }
