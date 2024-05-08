d36b56069e70647895a62ff78d8435dbf7714ddb
Bailey <baileyspraggins@me.com>
2024-05-08 17:55:39 -0500

    Simplified deployment scripts

--- test/mock/MockERC20.sol
+++ test/mock/MockERC20.sol
@@ -12,7 +12,11 @@ pragma solidity 0.8.23;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(string memory name, string memory symbol, uint8 d) ERC20(name, symbol, d) {}
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 d
+    ) ERC20(name, symbol, d) {}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
